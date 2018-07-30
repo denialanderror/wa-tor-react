012ceffa231c347813a675edc0408b36803cef2b
Sam Joseph <sam.joseph@blackcatsolutions.co.uk>
2018-07-30 10:08:20 +0100

    Hopefully reversed everything I did wrong now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './register-service-worker'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
